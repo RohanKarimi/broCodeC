6130dc9077c55c8cf33540d7c4fab4e0e9e93a68
Gabriel Tsurushima <protsuru@gmail.com>
2022-01-21 21:37:33 -0300

    string functions v2

--- 26-stringF.c
+++ 26-stringF.c
@@ -13,5 +13,13 @@ int main (void){
     strcpy(string1, string2);       // copy string2 to string1
     strncpy(string1, string2, 4)    // copy n characters of string2 to string1
 
-    
+    strset(string1, '?');           // sets all characters of a string to a given character
+    strnset(string1, 'x', 1);       // sets first n characters of a string to a given character
+    strrev(string1);                // reverses a string
+
+    strlen(string1);                // returns string length as int
+    strcmp(string1, string2);       // compare all characters, check if is the same
+    strncmp(string1, string2, 1);   // compare n characters
+    strcmpi(string1, string1);      // string compare all (ignore case)
+    strnicmp(string1, string1, 1);  // string compare n characters (ignore case)
 }
