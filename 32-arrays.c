dbca27cfb398bc4b68f0f36ab5900fc4a8438efb
Gabriel Tsurushima <protsuru@gmail.com>
2022-01-22 12:27:16 -0300

    manipulating arrays with for loops

---
+++ 32-arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // array = a data structure that can store many values of the same data type.
+
+    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0};
+
+    for (int i = 0; i < 5; i++)
+    {
+        
+    }
+
+    return 0;
+}
