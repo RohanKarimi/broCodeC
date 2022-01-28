d933ccc038608b27af13f6db8fa5d6f6ae2e4bd8
Gabriel Tsurushima <protsuru@gmail.com>
2022-01-28 15:37:39 -0300

    Use typedef to better sintaxe of code

--- 40-array_structs.c
+++ 40-array_structs.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Student
+typedef struct
 {
 	char name[12];
 	float gpa;
-};
+}Student;
 
 int main(void)
 {
 
-	struct Student student1 = {"Spongebob", 3.0};
-	struct Student student2 = {"Patrick", 2.5};
-	struct Student student3 = {"Sandy", 3.5};
-	struct Student student4 = {"Squidward", 4.0};
+	Student student1 = {"Spongebob", 3.0};
+	Student student2 = {"Patrick", 2.5};
+	Student student3 = {"Sandy", 3.5};
+	Student student4 = {"Squidward", 4.0};
 
-	struct Student students[] = {student1, student2, student3, student4};
+	Student students[] = {student1, student2, student3, student4};
 
 	int i = 0;
 	while(i < sizeof(students) / sizeof(students[0]))
