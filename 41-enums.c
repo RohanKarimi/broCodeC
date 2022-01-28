297f4a537349e795c0a3617bcc424cd9c7e6a02d
Gabriel Tsurushima <protsuru@gmail.com>
2022-01-28 15:53:35 -0300

    Make the program more readable

--- 41-enums.c
+++ 41-enums.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 enum Day{Sunday = 1, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday}; 
-// assigning the first element some value, left elements will agree the value
+// assigning the first element some value, left elements will act likely
+
 int main(void)
 {
-	// enum = a user defined type of named integer identifiers
-	// helps to make a program more readable
+	/* enum = a user defined type of named integer identifiers
+		 	  helps to make a program more readable */
 
 	enum Day today = Sunday;
 
-	printf("%d", today);
+	// printf("%d", today); enums is NOT STRING, but it can be treated as int
+
+	if(today == Sunday || today == Saturday)
+	{
+		printf("It's the weekend! Party time!");
+	}
+	else 
+	{
+		printf("I have to work today :(");
+	}
 
 	return 0;
 }
